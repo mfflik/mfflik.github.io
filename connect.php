610fd828c8af406df76ea9562ee68807b51977d2
mfflik <muh.fauzan.f@gmail.com>
2020-03-05 09:44:13 +0700

    Revert "Revert "Revert "Update connect.php"""
    This reverts commit 553b37748a5f554b382d48ed873a7592527b1cbc.

--- connect.php
+++ connect.php
@@ -1,5 +1,3 @@
 <?php
-    /* $connect = mysqli_connect('remotemysql.com','zErTqfLtZH','wBaq2TwkYW','zErTqfLtZH'); */
-    $connect = mysqli_connect('sql12.freesqldatabase.com','sql12323774','byynjWtUqV','sql12323774');
-
-	?>
+    $connect = mysqli_connect('remotemysql.com','zErTqfLtZH','wBaq2TwkYW','zErTqfLtZH');
+?>
